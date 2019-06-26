5e310e07e734b98e55fdc7266b49208d7f2e0edb
Shinyhunter2109 <35253149+Shinyhunter2109@users.noreply.github.com>
2019-06-26 09:30:17 +0200

    some small moveset updates

---
+++ Discord-Bot.py
@@ -0,0 +1,407 @@
+import discord
+import random
+import logging
+import traceback
+from discord.ext import commands, tasks
+from discord.utils import get
+import youtube_dl
+import os
+from discord import Spotify
+from itertools import cycle
+
+TOKEN = 'INSERT TOKEN HERE!'
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+client = commands.Bot(command_prefix = '.')
+client.remove_command('help')
+status = cycle(['Super Smash Bros Ultimate', 'GTS Giveaway'])
+
+
+@client.event
+async def on_ready():
+    change_status.start()
+    print('Logged in as: ' + client.user.name + '\n')
+    print('This Bot is Made by twitch.tv/shinyhunter2109')
+    print('//Bot version: 1.2//')
+
+
+@client.event
+async def on_error(event, *args, kwargs):
+    message = args[0]
+    logging.warning(traceback.format_exc())
+    await client.send_message(message.channel, "You caused an error!")
+
+
+class Slapper(commands.Converter):
+    async def convert(self, ctx, argument):
+        to_slap = random.choice(ctx.guild.members)
+        return '{0.author} slapped {1} because *{2}*'.format(ctx, to_slap, argument)
+
+
+@client.command()
+async def slap(ctx, *, reason: Slapper):
+    await ctx.send(reason)
+
+
+@client.command(pass_context=True)
+async def help(ctx):
+    author = ctx.message.author
+
+    embed = discord.Embed(
+        colour = discord.Colour.orange()
+    )
+
+    embed.set_author(name='help')
+    embed.add_field(name='.ping', value='Returns Pong!', inline=False)
+
+    await client.send_message(author, embed=embed)
+
+
+@client.command()
+async def spotify(ctx, user: discord.Member=None):
+    user = user or ctx.author
+    for activity in user.activities:
+        if isinstance(activity, Spotify):
+            await ctx.send(f'{user} is listening to {activity.title} by {activity.artist}')
+
+
+@client.command(pass_context=True, aliases=['j', 'joi'])
+async def join(ctx):
+    global voice
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+
+        await voice.disconnect()
+
+        if voice and voice.is_connected():
+            await voice.move_to(channel)
+        else:
+            voice = await channel.connect()
+            print(f'The bot has connected to {channel}\n')
+
+            await ctx.send(f'Joined {channel}')
+
+
+@client.command(pass_context=True, aliases=['l', 'lea'])
+async def leave(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.disconnect()
+        print(f'The bot has left {channel}')
+        await ctx.send(f'Left {channel}')
+    else:
+        print('Bot was told to leave voice channel, but was not in one')
+        await ctx.send('I am not in a voice channel')
+
+
+@client.command(pass_context=True, aliases=['p', 'pla'])
+async def play(ctx, url: str):
+    song_there = os.path.isfile('song.mp3')
+    try:
+        if song_there:
+            os.remove('song.mp3')
+            print('Removed old song file')
+    except PermissionError:
+        print('Trying to delete song file, but its being played')
+        await ctx.send('Error: Music playing')
+
+        await ctx.send('Getting everything ready now')
+
+        voice = get(client.voice_clients, guild=ctx.guild)
+
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            print('Downloading audio now\n')
+            ydl.download([url])
+
+            for file in os.listdir('./'):
+                if file.endswith('mp3'):
+                    name = file
+                    print(f'Renamed File: {file}\n')
+                    os.rename(file, 'song.mp3')
+
+                    voice.play(discord.FFmpegPCMAudio('song.mp3'), after=lambda e: print(f'{name} has finished playing'))
+                    voice.source = discord.PCMVolumeTransformer(voice.source)
+                    voice.source.volume = 0.07
+
+                    nname = name.rsplit('-', 2)
+                    await ctx.send(f'Playing: {nname}')
+                    print(f'playing\n')
+
+
+@client.command()
+async def coinflip(ctx):
+    choices = ['Heads', 'Tails']
+    rancoin = random.choice(choices)
+    await ctx.send(rancoin)
+
+
+@client.command()
+async def info(ctx, *, member: discord.Member):
+    fmt = '{0} joined on {0.joined_at} and has {1} roles.'
+    await ctx.send(fmt.format(member, len(member.roles)))
+
+
+@info.error
+async def info_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send('I could not find that member...')
+
+
+@tasks.loop(seconds=60)
+async def change_status():
+    await client.change_presence(activity=discord.Game(next(status)))
+
+
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+
+
+@client.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f'Unbanned {user.mention}')
+            return
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong!')
+
+
+@client.command()
+async def pokedex(ctx):
+    await ctx.send(f'There are 807 Pokemon on the Pokedex!')
+
+
+@client.command()
+async def abomasnow(ctx):
+    await ctx.send(f'Ability: Soundproof  EVs: 92 HP / 252 SpA / 164 Spe  Nature: Mild  Moves: Blizzard  Giga Drain  Focus Blast  Ice Shard  Item: Abomasite')
+
+
+@client.command()
+async def abra(ctx):
+    await ctx.send(f'Ability: Magic Guard  EVS: 236 Spa / 76 SpD / 196 Spe  Nature: Timid  Level: 5    Moves: Psychic  Dazzling Gleam  Hidden Power Fighting  Protect  Item: Focus Sash')
+
+
+@client.command()
+async def absol(ctx):
+    await ctx.send(f'Ability: Justified  EVS: 252 Atk / 4 Def / 252 Spe  Nature: Adamant  Moves: Swords Dance  Knock Off  Sucker Punch  Superpower  Item: Life Orb')
+
+
+@client.command()
+async def accelgor(ctx):
+    await ctx.send(f'Ability: Sticky Hold  EVS: 4 Def / 252 SpA / 252 Spe  Nature: Timid  Moves: Bug Buzz  Focus Blast  Energy Ball  Spikes  Item: Choice Specs')
+
+
+@client.command()
+async def aegislash(ctx):
+    await ctx.send(f'Ability: Stance Change  EVS: 252 HP / 252 SpA / 4 SpD  Nature: Quit  Moves: Shadow Ball  Flash Cannon  Shadow Sneak  Kings Shield  Item: Leftovers')
+
+
+@client.command()
+async def aerodactyl(ctx):
+    await ctx.send(f'Ability: Unnerve  EVS: 252 Atk / 4 Def / 252 Spe  Nature: Jolly  Moves: Stone Edge  Earthquake  Pursuit  Roost  Item: Aerodactylite')
+
+
+@client.command()
+async def aggron(ctx):
+    await ctx.send(f'Ability: Sturdy  EVS: 252 HP / 4 Def / 252 SpD  Nature: Careful  Moves: Stealth Rock  Heavy Slam  Earthquake  Toxic  Item: Aggronite')
+
+
+@client.command()
+async def aimpom(ctx):
+    await ctx.send(f'Ability: Skill Link  EVS: 76 HP / 116 Atk / 76 Def / 236 Spe  Nature: Jolly  Level: 5    Moves: Fury Swipes  Knock Off  Brick Break  Fake Out  Item: Life Orb')
+
+
+@client.command()
+async def alakazam(ctx):
+    await ctx.send(f'Ability: Magic Guard  EVS: 4 Def / 252 SpA / 252 Spe  Nature: Timid  Moves: Psychic  Focus Blast  Recover  Shadow Ball  Item: Alakazite')
+
+
+@client.command()
+async def alomomola(ctx):
+    await ctx.send(f'Ability: Regenerator  EVS: 40 HP / 252 Def / 216 SpD  Nature: Bold  Moves: Wish  Protect  Toxic  Scald  Item: Leftovers')
+
+
+@client.command()
+async def altaria(ctx):
+    await ctx.send(f'Ability: Natural Cure  EVS: 72 HP / 252 Atk / 184 Spe  Nature: Adamant  Moves: Dragon Dance  Return  Refresh  Roost  Item: Altarianite')
+
+
+@client.command()
+async def amaura(ctx):
+    await ctx.send(f'Ability: Snow Warning  EVS: 60 HP / 220 SpA / 228 Spe  Nature: Modest  Level: 5    Moves: Blizzard  Earth Power  Thunderbolt  Ancient  Item: Choice Scarf')
+
+
+@client.command()
+async def ambipom(ctx):
+    await ctx.send(f'Ability: Technican  EVS: 252 Atk / 4 Def / 252 Spe  Nature: Jolly  Moves: Fake Out  Return  Low Kick  U-turn  Item: Life Orb')
+
+
+@client.command()
+async def amoonguss(ctx):
+    await ctx.send(f'Ability: Regenerator  EVS: 252 HP / 176 Def / 80 SpD  Nature: Bold  Moves: Spore  Giga Drain  Hidden Power Fire  Clear Smog  Item: Rocky Helmet')
+
+
+@client.command()
+async def ampharos(ctx):
+    await ctx.send(f'Ability: Static  EVS: 4 HP / 252 SpA / 252 Spe  Nature: Modest  Moves: Volt Switch  Dragon Pulse  Focus Blast  Thunderbolt  Item: Ampharosite')
+
+
+@client.command()
+async def anorith(ctx):
+    await ctx.send(f'Ability: Battle Armor  EVS: 236 Atk / 36 Def / 236 Spe  Nature: Jolly  Level: 5  Moves: Stealth Rock  Rapid Spin  Knock Off  Rock Blast  Item: Berry Juice')
+
+
+@client.command()
+async def araquanid(ctx):
+    await ctx.send(f'Ability: Water Bubble  EVS: 96 HP / 220 Atk / 192 Spe  Nature: Adamant  Moves: Liquidation  Spider Web  Toxic  Rest  Item: Splash Plate')
+
+
+@client.command()
+async def arbok(ctx):
+    await ctx.send(f'Ability: Intimidate  EVS: 252 Atk / 4 SpD / 252 Spe  Nature: Adamant  Moves: Coil  Gunk Shot  Earthquake  Sucker Punch  item: Black Sludge')
+
+
+@client.command()
+async def arcanine(ctx):
+    await ctx.send(f'Ability: Flash Fire  EVS: 252 Atk / 4 Def / 252 Spe  Nature: Jolly  Moves: Flare Blitz  Wild Charge  Extreme Speed  Morning Sun  Item: Life Orb')
+
+
+@client.command()
+async def arceus(ctx):
+    await ctx.send(f'Ability: Multitype  EVS: 240 HP / 252 Atk / 16 Spe  Nature: Adamant  Moves: Swords Dance  Extreme Speed  Shadow Claw  Recover  Item: Chople Berry')
+
+
+@client.command()
+async def archen(ctx):
+    await ctx.send(f'Ability: Defeatist  EVS: 76 HP / 180 Atk / 196 Spe  Nature: Jolly  Level: 5  Moves: Acrobatics  Rock Slide  Heat Wave  Hidden Power Grass Item: Berry Juice')
+
+
+@client.command()
+async def archeops(ctx):
+    await ctx.send(f'Ability: Defeatist  EVS: 252 Atk / 252 Spe / 0 HP / 0 Def / 0 SpD  Nature: Naive  Moves: Head Smash  Stealth Rock  Endeavor  Taunt  Item: Focus Sash')
+
+
+@client.command()
+async def ariados(ctx):
+    await ctx.send(f'Ability: Swarm  EVS: 252 Atk / 4 Def / 252 Spe  Nature: Jolly  Moves: Sticky Web  Toxic Spikes  Megahorn  Toxic Thread  Item: Focus Sash')
+
+
+@client.command()
+async def armaldo(ctx):
+    await ctx.send(f'Ability: Battle Armor  EVS: 252 HP / 92 Atk / 164 Spe  Nature: Adamant  Moves: Rapid Spin  Stone Edge  Knock Off  Earthquake  Item: Leftovers')
+
+
+@client.command()
+async def aromatisse(ctx):
+    await ctx.send(f'Ability: Aroma Veil  EVS: 248 HP / 252 SpA / 8 SpD  Nature: Quit  Moves: Trick Room  Nasty Plot  Moonblast  Psychic  Item: Fairium Z')
+
+
+@client.command()
+async def aron(ctx):
+    await ctx.send(f'Ability: Rock Head  EVS: 196 Atk / 116 SpD / 196 Spe  Nature: Jolly  Level: 5  Moves: Rock Polish  Head Smash  Heavy Slam  Earthquake  Item: Eviolite')
+
+
+@client.command()
+async def articuno(ctx):
+    await ctx.send(f'Ability: Pressure  EVS: 252 SpA / 4 SpD / 252 Spe  Nature: Timid  Moves: Substitute  Roost  Freeze-Dry  Hurricane  Item: Leftovers')
+
+
+@client.command()
+async def audino(ctx):
+    await ctx.send(f'Ability: Regenerator  EVS: 252 HP / 4 Def / 252 SpD  Nature: Calm  Moves: Wish  Protect  Heal Bell  Knock Off  Item: Audinite')
+
+
+@client.command()
+async def aurorus(ctx):
+    await ctx.send(f'Ability: Snow Warning  EVS: 252 SpA / 4 SpD / 252 Spe  Nature: Modest  Moves: Blizzard  Freeze-Dry  Earth Power  Hidden Power Rock  Item: Choice Specs')
+
+
+@client.command()
+async def avalugg(ctx):
+    await ctx.send(f'Ability: Sturdy  EVS: 252 HP / 88 Atk / 168 Def  Nature: Impish  Moves: Avalanche  Recover  Rapid Spin  Earthquake  Item: Rocky Helmet')
+
+
+@client.command()
+async def axew(ctx):
+    await ctx.send(f'Ability: Mold Breaker  EVS: 68 HP / 220 Atk / 220 Spe  Nature: Jolly  Moves: Dragon Dance  Outrage  Superpower  Iron Tail  Item: Eviolite')
+
+
+@client.command()
+async def azelf(ctx):
+    await ctx.send(f'Ability: Levitate  EVS: 252 Atk / 4 SpA / 252 Spe  Nature: Jolly  Moves: Stealth Rock  Explosion  Taunt  Knock Off  Item: Focus Sash')
+
+
+@client.command()
+async def azumarill(ctx):
+    await ctx.send(f'Ability: Huge Power  EVS: 252 Atk / 4 HP / 252 Spe  Nature: Adamant  Moves: Belly Drum  Aqua Jet  Play Rough  Knock Off  Item: Sitrus Berry')
+
+@client.command()
+async def azurill(ctx):
+    await ctx.send(f'Ability: Huge Power  EVS: 196 HP / 196 Atk / 116 Def  Nature: Brave  Level: 5  Moves: Double-Edge  Waterfall  Return  Knock Off  Item: Life Orb')
+
+
+@client.command(aliases=['8ball', 'test'])
+async def _8Ball(ctx, *, question):
+    responces = ['It is certain.',
+                 'It is decidedly so.',
+                 'Without a doubt',
+                 'Yes.',
+                 'My reply is no.',
+                 'Outlook not so good.',
+                 'Very doubtful.']
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responces)}')
+
+
+@client.command()
+async def clear(ctx, amount=100):
+    await ctx.channel.purge(limit=amount)
+
+
+@client.event
+async def on_member_join(member):
+    role = discord.utils.get(member.guild.roles, name='INSERT ROLe HERE!')
+    await client.add_roles(member.name, role)
+    print(f'{member} has joined a server.')
+
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left a server.')
+
+client.run(TOKEN)
