006aec55f4d3359aff620ac136e38ff25043828b
Shinyhunter2109 <35253149+Shinyhunter2109@users.noreply.github.com>
2020-08-29 17:05:26 +0200

    added YouTube Download Function

--- Getting Started/start.bat
+++ Getting Started/start.bat
@@ -1,3 +1,3 @@
-pip install discord.py[Voice] youtube_dl PyNaCL asyncio pandas PyGithub twitch-python
+pip install discord.py[Voice] youtube_dl PyNaCL asyncio pandas PyGithub twitch-python pytube
 
 pause
