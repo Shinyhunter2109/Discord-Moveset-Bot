b9a0c5ca97ccdc17eef03e20b2459e3a5732cc43
Shinyhunter2109 <35253149+Shinyhunter2109@users.noreply.github.com>
2020-08-29 17:06:10 +0200

    added YouTube Download Function

--- Getting Started/Updater for Dependencies/updater.bat
+++ Getting Started/Updater for Dependencies/updater.bat
@@ -1,3 +1,3 @@
-pip install discord.py[Voice] youtube_dl PyGithub PyNaCL pandas twitch-python --upgrade
+pip install discord.py[Voice] youtube_dl PyGithub PyNaCL pandas twitch-python pytube --upgrade
 
 pause
